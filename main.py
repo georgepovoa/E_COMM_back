87f020d27fc5d0b8a040ccb0c4925ebc7b029f5a
georgeceub <georgepovoa12@gmail.com>
2022-08-01 09:12:03 -0300

    PRA DEPLOY

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from fastapi import FastAPI
 from pymongo import MongoClient
 from pydantic import BaseModel, Field
@@ -27,6 +28,15 @@ class User(BaseModel):
     _id:Union[str, None] = None
     nome:str
     email:str
+
+class Mensagem(BaseModel):
+    _id:Union[str, None] = None
+    nome:str
+    email:str
+    assunto:str
+    mensagem:str
+
+
 
 class Produto(BaseModel):
     _id:Union[str, None] = None
@@ -58,13 +68,20 @@ client = MongoClient(CONNECTION_STRING)
 db = client['eshop']
 products_col = db["products_col"] 
 carrinho_col = db["carrinho_col"] 
+message_col = db["message_col"] 
 
 
 @app.get("/")
 async def root():
-
-    
     return {"message": "Hello World"}
+
+
+@app.post('/produtos/many')
+async def insert_product(produto:list):
+
+    products_col.insert_many(produto)
+    
+    return produto 
 
 @app.post('/produtos')
 async def insert_product(produto:Produto):
@@ -76,7 +93,7 @@ async def insert_product(produto:Produto):
 
 @app.get('/produtos/all')
 def get_product():
-    products_find = products_col.find()
+    
     return list(products_col.find())
 
 @app.get('/carrinhos/{user}')
@@ -102,13 +119,36 @@ async def post_carrinho(carrinho:Carrinho):
 
 @app.put("/carrinhos/add/{user}/{id}")
 async def add_to_carrinho(user:str,id:str):
+    carrinho_produtos = carrinho_col.find_one({"user":user},{'produtos':1})
+    print(carrinho_produtos)
+    if id in carrinho_produtos["produtos"]:
+        return {"Warning":"Produto já existe"}
     carrinho_col.update_one({'user':user},{'$push':{'produtos':id}},upsert=True)
     return {"OK":"OK"}
 
 @app.put("/carrinhos/remove/{user}/{id}")
-async def add_to_carrinho(user:str,id:str):
+async def remove_from_carrinho(user:str,id:str):
     carrinho_col.update_one({'user':user},{'$pull':{'produtos':id}},upsert=True)
     return {"OK":"OK"}
+
+@app.get("/mensagem")
+async def get_messages():
+    mensagens = message_col.find()
+    print(mensagens.__dict__)
+    return list(mensagens)
+
+@app.post("/mensagem")
+async def post_messages(mensage:Mensagem):
+    mensage = mensage.__dict__
+    mensage["_id"] = str(uuid.uuid4())
+    mensagens = message_col.insert_one(mensage)
+    return mensage
+
+
+
+
+
+
 
 
 
